d39afd947f62a9868e123af02b395e396963118c
Transcend Zunaid <Transcend Zunaid@DESKTOP-7RDU8GT>
2023-12-12 11:00:02 +0530

    Add project files.

---
+++ EmployeeCrud/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using EmployeeCrud.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeCrud.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ EmployeeCrud/Models/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeCrud.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Employee Name Is required")]
+        [Display(Name = "Employee Name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Designation Name Is required")]
+        public string Designation { get; set; }
+
+
+        [Required(ErrorMessage = "Address Name Is required")]
+        [DataType(DataType.MultilineText)]
+        public string Address { get; set; }
+
+        [Required(ErrorMessage = "Date Is required")]
+        public DateTime? RecordCreatedOn { get; set; }
+    }
+}
